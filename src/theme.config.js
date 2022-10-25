d1be6b63468b506f983de3fc4794ee534ee96920
Ryan Shea <ryan@shea.io>
2022-10-25 16:38:31 -0400

    minor updates

--- src/theme.config.js
+++ src/theme.config.js
@@ -89,7 +89,7 @@ export default {
           { url: frontMatter.image || 'https://www.micro-btc.dev/og.png' }
         ]
       },
-      titleTemplate: '%s – micro-btc',
+      titleTemplate: 'micro-btc - %s',
       twitter: {
         cardType: 'summary_large_image',
         site: 'https://www.micro-btc.dev'
